2d6ad071a568907b3c11e81d3da8c7edd3c1c54d
pratishruti1234 <pratishrutipanda0102@gmail.com>
2020-09-03 13:27:04 +0530

    my commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Country from './Country';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <Country></Country>,
   document.getElementById('root')
 );
 
